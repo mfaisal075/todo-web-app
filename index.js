7362f9dc1898908504d8e6e9060adc4cbc027672
Mian Faisal <fhafiz397@gmail.com>
2024-10-15 17:09:29 +0500

    change vercel.js and make build

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import { registerRootComponent } from 'expo';
 
-
+// Web-specific code
+const rootTag = document.getElementById('app-root') || document.getElementById('root');
 AppRegistry.registerComponent(appName, () => App);
-
 AppRegistry.runApplication(appName, {
-    initialProps: {},
-    rootTag: document.getElementById('app-root'),
-  });
+  initialProps: {},
+  rootTag,
+});
 
-  registerRootComponent(App);
+registerRootComponent(App);
